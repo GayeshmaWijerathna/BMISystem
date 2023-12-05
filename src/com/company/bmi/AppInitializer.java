93949bc4a058b3b3552342aab3cdb381ee77806a
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2023-12-06 03:29:45 +0530

    Add some Changes

--- src/com/company/bmi/AppInitializer.java
+++ src/com/company/bmi/AppInitializer.java
@@ -18,6 +18,7 @@ public class AppInitializer extends Application {
         Scene scene=new Scene(load);
         primaryStage.setTitle("BMI Calculator");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     public static void main(String[] args) {launch(args);
